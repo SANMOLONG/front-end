791054b12da8dc146f25f52d1b1c30e7ac1a2f4b
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-09-22 02:12:37 +0900

    [refactor]오타수정 및 router 연결

--- src/hooks/useNavigate.ts
+++ src/hooks/useNavigate.ts
@@ -1,15 +1,15 @@
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 
 type useRouteType = {
-  onNavigete: (url:string) => () => void
-  id: string | undefined
-}
+	onNavigate: (url: string) => () => void;
+	id: string | undefined;
+};
 
-export const useRoute = ():useRouteType => {
-  const navigete = useNavigate()
-  const {id} = useParams()
-  const onNavigete = (url:string) => ()=> {
-    navigete(url)
-  }
-  return {onNavigete, id}
-}
+export const useRoute = (): useRouteType => {
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const onNavigate = (url: string) => () => {
+		navigate(url);
+	};
+	return { onNavigate, id };
+};
